88d805062a0a9d6f2db680caf1376f143c29cb47
abdilince91-a11y <abdil.ince91@gmail.com>
2026-09-21 19:48:14 +0200

    feat(merchant): add pure proposal preview integration

---
+++ src/lib/merchant/google/index.ts
@@ -0,0 +1,16 @@
+export { createGoogleMerchantAdapter } from "./adapter";
+export type { GoogleMerchantAdapter } from "./adapter";
+export { mapOffer, mapOffers } from "./mapper";
+export { toFeedUpdateProposal } from "./proposal";
+export type { PreviewMeta } from "./proposal";
+export type {
+  BatchMapResult,
+  InvalidOffer,
+  MapOfferResult,
+  MappedOffer,
+  MerchantChannelConfig,
+  MerchantOfferInput,
+  MerchantProductInput,
+  SkippedOffer,
+} from "./types";
+export type { MerchantIssue } from "./errors";
